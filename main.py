Another Big Fix: 1. GAE calculation going past episode bounds 2. Clipped values calculation 3. Training not using previously sampled    actions 4. Masking using the non-in-place function 5. OOB states now considered terminal instead of    truncated

main.py
@@ -42,7 +42,7 @@ for i in range(num_iterations):
     actions = []
     log_probs = []
     entropies = []
-    rwd_func = []
+    rewards = []
     ended = []
     vals = []
     vals_offset = []
@@ -59,11 +59,11 @@ for i in range(num_iterations):
         vals_offset.append(end_count)
 
         state, reward, terminated, truncated, info = env.step(action, found_charges, log_file)
-        rwd_func.append(reward)
+        rewards.append(reward)
 
         ended.append(int(terminated or truncated))
 
-        if terminated or truncated or j == num_transitions - 1:  # Last non terminal / truncated state also requires the next value
+        if ended[-1] or j == num_transitions - 1:  # Last non terminal / truncated state also requires the next value
             state = torch.tensor(state, dtype=torch.float32)
             vals.append(int(not terminated) * agent.get_action_val(torch.unsqueeze(state, 0), value_only=True))   # Terminated states will hvae zero value
 
@@ -80,7 +80,7 @@ for i in range(num_iterations):
     agent.log_probs = torch.stack(log_probs).detach()
     agent.vals = torch.stack(vals).detach()
     agent.vals_offset = torch.tensor(vals_offset)
-    agent.rewards = torch.tensor(rwd_func)
+    agent.rewards = torch.tensor(rewards)
     agent.ended = torch.tensor(ended)
 
     # agent.rewards = ((agent.rewards - torch.mean(agent.rewards)) / torch.std(agent.rewards)).detach()
@@ -106,7 +106,7 @@ for i in range(num_iterations):
     val_losses.append((batch_val_loss / num_epochs).detach())
     tot_losses.append((batch_tot_loss / num_epochs).detach())
 
-    print(f"End of itetration\nNumber of solutions found so far: {len(found_charges)}")
+    print(f"End of iteration\nNumber of solutions found so far: {len(found_charges)}")
 
 log_file.close()
 

src/neural_net.py
@@ -101,8 +101,7 @@ class PPO():
 
         # For dependent 3rd gen charges
         generation_mask = torch.concat((torch.zeros((states.size()[0], 2), dtype=torch.bool),
-                                        torch.unsqueeze(chosen_particle >= 2, 1)),
-                                        dim=-1)
+                                        torch.unsqueeze(chosen_particle >= 2, dim=-1)), dim=-1)
         generation_logits.masked_fill_(generation_mask, -torch.inf)
 
         generation_distr = torch.distributions.Categorical(logits=generation_logits)
@@ -111,24 +110,54 @@ class PPO():
 
         # To prevent going oob
         # Note that the 3rd gen charges CAN STILL EXCEED the bounds
-        mod_mask = torch.stack((states[torch.unsqueeze(torch.arange(states.size()[0]), 0),
+        mod_mask = torch.stack((states[torch.arange(states.size()[0]),
                                        chosen_particle, chosen_generation] <= -self.max_charge,
-                                states[torch.unsqueeze(torch.arange(states.size()[0]), 0),
+                                states[torch.arange(states.size()[0]),
                                        chosen_particle, chosen_generation] >= self.max_charge),
                                 dim=-1)
-        mod_logits.masked_fill(mod_mask, -torch.inf)
+        mod_logits.masked_fill_(mod_mask, -torch.inf)
 
         mod_distr = torch.distributions.Categorical(logits=mod_logits)
         chosen_mod = mod_distr.sample()
         mod_log_prob = mod_distr.log_prob(chosen_mod)
 
-        ret = (torch.stack((chosen_particle, chosen_generation, chosen_mod)),
+        ret = (torch.tensor((chosen_particle.item(), chosen_generation.item(), chosen_mod.item())),
                torch.sum(torch.stack((particle_log_prob, generation_log_prob, mod_log_prob)), 0)) + ret
         if get_entropy:
             ret += (torch.mean(torch.stack((particle_distr.entropy(), generation_distr.entropy(), mod_distr.entropy()))),)
         return ret  # I got sick of writing 5 billions ifs
 
-    def calc_gae_tar(self):     # haha gay
+    def get_action_log_probs(self, states, actions, get_entropy=True):
+        particle_logits, generation_logits, mod_logits = self.actor_critic.forward(states, action_only=True)
+        particle_distr = torch.distributions.Categorical(logits=particle_logits)
+        chosen_particle = actions[:, 0]
+        particle_log_prob = particle_distr.log_prob(chosen_particle)
+
+        generation_mask = torch.concat((torch.zeros((states.size()[0], 2), dtype=torch.bool),
+                                        torch.unsqueeze(chosen_particle >= 2, dim=-1)), dim=-1)
+        generation_logits.masked_fill_(generation_mask, -torch.inf)
+
+        generation_distr = torch.distributions.Categorical(logits=generation_logits)
+        chosen_generation = actions[:, 1]
+        generation_log_prob = generation_distr.log_prob(chosen_generation)
+
+        mod_mask = torch.stack((states[torch.arange(states.size()[0]),
+                                       chosen_particle, chosen_generation] <= -self.max_charge,
+                                states[torch.arange(states.size()[0]),
+                                       chosen_particle, chosen_generation] >= self.max_charge),
+                                dim=-1)
+        mod_logits.masked_fill(mod_mask, -torch.inf)
+
+        mod_distr = torch.distributions.Categorical(logits=mod_logits)
+        chosen_mod = actions[:, 2]
+        mod_log_prob = mod_distr.log_prob(chosen_mod)
+
+        if get_entropy:
+            return (torch.sum(torch.stack((particle_log_prob, generation_log_prob, mod_log_prob)), 0),
+                    torch.mean(torch.stack((particle_distr.entropy(), generation_distr.entropy(), mod_distr.entropy()))))
+        return torch.sum(torch.stack((particle_log_prob, generation_log_prob, mod_log_prob)), 0)
+
+    def calc_gae_tar(self):          # haha gay
         j = self.vals.size()[0] - 3  # index for values since its size depends on the number of terminal and truncated states
 
         self.advantages[-1] = self.rewards[-1] + self.gamma*self.vals[-1] - self.vals[-2]
@@ -138,7 +167,7 @@ class PPO():
                 j -= 1
 
             delta = self.rewards[i] + self.gamma*self.vals[j+1] - self.vals[j]
-            self.advantages[i] = self.gamma*self.lmbda*self.advantages[i+1] + delta
+            self.advantages[i] = self.gamma*self.lmbda*(1-self.ended[i])*self.advantages[i+1] + delta
             self.vals_tar[i] = torch.add(self.advantages[i], self.vals[j])
 
             j -= 1
@@ -171,8 +200,7 @@ class PPO():
             # For vals to match with states and therefore new_vals,
             # its indices should be 0, 1, 3, 4, 6 as opposed to 0, 1, 2, 3, 4
 
-            if self.ended[i]:
-                j -= 1
+            # looking back, i genuinely dont know why i kept the j; it makes no sense
 
             if new_vals[j] < self.vals[i+self.vals_offset[i]] - self.val_clip_epsilon:
                 clip_vals[j] = self.vals[i+self.vals_offset[i]] - self.val_clip_epsilon
@@ -180,8 +208,6 @@ class PPO():
                 clip_vals[j] = self.vals[i+self.vals_offset[i]] + self.val_clip_epsilon
             else:
                 clip_vals[j] = new_vals[j]
-
-            j += 1
 
         return clip_vals
 
@@ -195,7 +221,10 @@ class PPO():
             start = j * self.minibatch_size
             end = (j+1) * self.minibatch_size
 
-            _, new_log_probs, new_vals, entropy = self.get_action_val(self.states[indices[start:end]], get_entropy=True)
+            new_log_probs, entropy = self.get_action_log_probs(self.states[indices[start:end]],
+                                                               self.actions[indices[start:end]],
+                                                               get_entropy=True)
+            new_vals = self.get_action_val(self.states[indices[start:end]], value_only=True)
             ratio = torch.exp(torch.sub(new_log_probs, torch.flatten(self.log_probs[indices[start:end]])))
             obj = self.get_clip_obj(ratio, indices[start:end])
     