f9afe155a6a20c124d14bb4d3030277c72703a19
Fish <huirutong06@gmail.com>
2026-09-22 12:34:06 +0800

    Standardised Reward Functions They all have roughly the same structure now

--- src/rwd_func.py
+++ src/rwd_func.py
@@ -7,40 +7,49 @@ def gen_rwd(found_charges, curr_charges, curr_coef, prev_coef):
         Gain some reward for each coef that's 0
     """
     r = 0
+    is_valid = 1
+
     sorted_charges = get_sorted_charges(curr_charges)
     """for found in found_charges:
         if multiple_check(found, sorted_charges):
             return -50, False"""
 
     for i in range(3):
-        if not curr_coef[i]:
+        if curr_coef[i]:
+            is_valid = 0
+        else:
             r += 5
-    if r == 15:
+
+    if is_valid:
         found_charges.append(sorted_charges)
-        return 30, True
+        return 15, True
 
     return r, False
 
 def tot_improvement_rwd(found_charges, curr_charges, curr_coef, prev_coef):
     """
-        Gain a tiny reward if the current sum
+        Gain a reward if the current sum
         of coef is less than before and lose
         some if more than
     """
-    curr_tot_coef = np.sum(np.abs(curr_coef))
-    prev_tot_coef = np.sum(np.abs(prev_coef))
-    r = 0
+    is_valid = 1
 
     sorted_charges = get_sorted_charges(curr_charges)
+    curr_tot_coef = 0
+    prev_tot_coef = 0
 
-    if not curr_coef[0] and not curr_coef[1] and not curr_coef[2]:
+    for i in range(3):
+        if curr_coef[i]:
+            is_valid = 0
+
+        curr_tot_coef += np.abs(curr_coef[i] / 10**(i+1))
+        prev_tot_coef += np.abs(prev_coef[i] / 10**(i+1))
+    
+    if is_valid:
         found_charges.append(sorted_charges)
-        return 30, True
+        return 15, True
 
-    if curr_tot_coef < prev_tot_coef:
-        r += 9
-
-    return r, False
+    return 9 if curr_tot_coef < prev_tot_coef else -9, False
 
 def split_improvement_rwd(found_charges, curr_charges, curr_coef, prev_coef):
     """
@@ -49,21 +58,22 @@ def split_improvement_rwd(found_charges, curr_charges, curr_coef, prev_coef):
         any are more than
     """
     r = 0
+    is_valid = 1
 
     sorted_charges = get_sorted_charges(curr_charges)
     for i in range(3):
-        if not curr_coef[i]:
-            r += 5
-        elif abs(curr_coef[i]) < abs(prev_coef[i]):
-            r += 3
-    if r == 15:
+        if curr_coef[i]:
+            is_valid = 0
+            if abs(curr_coef[i]) < abs(prev_coef[i]):
+                r += 3
+
+    if is_valid:
         found_charges.append(sorted_charges)
-        return 30, True
+        return 15, True
         
     return r, False
 
 def abs_tot_err_rwd(found_charges, curr_charges, curr_coef, prev_coef):
-    r = 0
     is_valid = 1
 
     sorted_charges = get_sorted_charges(curr_charges)
@@ -71,20 +81,15 @@ def abs_tot_err_rwd(found_charges, curr_charges, curr_coef, prev_coef):
     prev_tot_coef = 0
 
     for i in range(3):
-        if curr_coef[i] == 0:
-            r += 5
-        else:
+        if curr_coef[i]:
             is_valid = 0
-            curr_tot_coef += np.abs(curr_coef[i] / 10**(i+1))
-            prev_tot_coef += np.abs(prev_coef[i] / 10**(i+1))
+
+        curr_tot_coef += np.abs(curr_coef[i] / 10**i)
+        prev_tot_coef += np.abs(prev_coef[i] / 10**i)
 
     if is_valid:
         found_charges.append(sorted_charges)
-        return r, True
-    
-    if not curr_coef[0] and not curr_coef[1] and not curr_coef[2]:
-        found_charges.append(sorted_charges)
-        return r, True
+        return 15, True
 
     return -curr_tot_coef + prev_tot_coef, False
 
@@ -99,14 +104,12 @@ def abs_err_rwd(found_charges, curr_charges, curr_coef, prev_coef):
     sorted_charges = get_sorted_charges(curr_charges)
 
     for i in range(3):
-        if curr_coef[i] == 0:
-            r += 5
-        else:
+        if curr_coef[i]:
             is_valid = 0
-            r += (-curr_coef[i] + prev_coef[i]) / 10**(i+1)
+            r += (-abs(curr_coef[i]) + abs(prev_coef[i])) / 10**i
 
     if is_valid:
         found_charges.append(sorted_charges)
-        return r, True
+        return 15, True
 
     return r, False
